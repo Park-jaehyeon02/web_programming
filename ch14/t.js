b63257e8cead83074f9753494d35d778c197a8ab
Park-jaehyeon02 <1234qkr@gmail.com>
2022-05-31 11:52:04 +0900

    add

---
+++ ch14/t.js
@@ -0,0 +1,8 @@
+function t1(obj){
+    obj.src="../resource/cat.gif"
+    document.writeln("<h3>Hi!</h3>");
+}
+function t2(obj){
+    obj.src="../resource/banner01.png";
+    document.write("<h3>Welcome!</h3>");
+}
